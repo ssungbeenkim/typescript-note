284d60dfd53d4f9d9323739306ca021b74393706
wandakim <nwd1583@naver.com>
2023-03-13 20:49:27 +0900

    Types: Type aliases

--- 1-types/1-4-alias.ts
+++ 1-types/1-4-alias.ts
@@ -1,4 +1,6 @@
 {
+  // 타입스크립트의 타입이 강력한 이유는 첫째로 바로 Type Aliase 때문이다.
+  // 새로운 타입을 정의할 수 있다.
   /**
    * Type Aliases
    */
@@ -7,12 +9,14 @@
   const address: Text = 'korea';
   type Num = number;
   type Student = {
+    // Object type
     name: string;
     age: number;
   };
   const student: Student = {
     name: 'ellie',
     age: 12,
+    // animal: dog // error
   };
 
   /**
@@ -22,7 +26,7 @@
   let ellieName: Name;
   ellieName = 'name';
   type JSON = 'json';
-  const json: JSON = 'json';
+  const json: JSON = 'json'; // 'json' 만 할당할 수 있음
 
   type Boal = true;
 }
