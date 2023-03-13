2481556af279a6046118d51f5360407f1d3dc103
wandakim <nwd1583@naver.com>
2023-03-13 21:24:55 +0900

    Types: Discriminated

--- 1-types/1-5-union.ts
+++ 1-types/1-5-union.ts
@@ -1,13 +1,13 @@
 {
   /**
-   *  Union Types: OR
+   *  Union Types: OR // 많이 쓰임. 활용도가 높다.
    */
   type Direction = 'left' | 'right' | 'up' | 'down';
   function move(direction: Direction) {
     console.log(direction);
   }
-  move('down');
-
+  move('down'); // ''입력하는 순간 위의 네가지 옵션이 보여짐
+  // 여러 케이스 중에 하나를 골라야 하는 경우에 쓸 수 있다.
   type TileSize = 8 | 16 | 32;
   const tile: TileSize = 16;
 
@@ -23,10 +23,11 @@
   type LoginState = SuccessState | FailState;
 
   function login(): LoginState {
+    //function login(id: string, password: string): Promise<LoginState> { 실제로는 이런식으로 쓸것.
     return {
       response: {
         body: 'logged in!',
-      },
+      }, // 예제가 조금 이해가 안가는데, 성공했다고 친것.
     };
   }
 
@@ -35,6 +36,7 @@
   // fail -> 😭 reason
   function printLoginState(state: LoginState) {
     if ('response' in state) {
+      // response라는 키가 state object안에 있다면, 이렇게 in 키워드도 많이 사용하지만 좋은 방법은 아니다.
       console.log(`🎉 ${state.response.body}`);
     } else {
       console.log(`😭 ${state.reason}`);
