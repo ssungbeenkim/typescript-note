ac26b627343774f0c058fad190904188f84d7404
wandakim <nwd1583@naver.com>
2023-03-14 14:24:11 +0900

    Types: enum

--- 1-types/1-8-enum.ts
+++ 1-types/1-8-enum.ts
@@ -1,6 +1,8 @@
 {
   /**
    * Enum
+   * 상수 값들을 하나로 모아서 정의할 수 있게 도와줌.
+   * JS에서는 지원하지 않아서 TS에서 자체적으로 지원
    */
   // JavaScript
   const MAX_NUM = 6;
@@ -12,22 +14,25 @@
   const dayOfToday = DAYS_ENUM.MONDAY;
 
   // TypeScript
-  type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday';
+  type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday'; // enum 보다는 union type을 활용하자.
   enum Days {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
+    Monday, //0 // Monday = 1로 지정하면 1부터 시작할 수 있다.
+    Tuesday, //1 // 문자열로도 할당할 수 있지만 전부 지정해주어야 한다.
+    Wednesday, //2
+    Thursday, //3
     Friday,
     Saturday,
     Sunday,
   }
-  console.log(Days.Monday);
+  console.log(Days.Tuesday);
   let day: Days = Days.Saturday;
-  day = Days.Tuesday;
-  day = 10;
+  // day = Days.Tuesday;
+  // day = 10; // TypeScript 에서는 enum을 잘 쓰지 않는 것이 좋다.
+  // 다른 어떤 숫자도 할당할 수 있고 에러가 발생하지 않고 타입 보장 불가능
   console.log(day);
 
   let dayOfweek: DaysOfWeek = 'Monday';
   dayOfweek = 'Wednesday';
+  // union type으로 충분히 대체되어 타입 보장하여 사용이 가능하다.
+  // 모바일 클라이언트와 의사소통 하는 등 꼭 필요한 경우가 아니라면 사용하지 않는 것이 좋다.
 }
