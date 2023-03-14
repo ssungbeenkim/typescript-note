b8243653fd0304595e7b44606b960df1f9ceaa63
wandakim <nwd1583@naver.com>
2023-03-14 15:23:19 +0900

    Types-projects: game

--- 2-types-projects/game.ts
+++ 2-types-projects/game.ts
@@ -23,14 +23,14 @@
         throw new Error(`unknown direction: ${direction}`);
     }
   }
-}
 
-console.log(position); // { x: 0, y: 0}
-move('up');
-console.log(position); // { x: 0, y: 1}
-move('down');
-console.log(position); // { x: 0, y: 0}
-move('left');
-console.log(position); // { x: -1, y: 0}
-move('right');
-console.log(position); // { x: 0, y: 0}
+  console.log(position); // { x: 0, y: 0}
+  move('up');
+  console.log(position); // { x: 0, y: 1}
+  move('down');
+  console.log(position); // { x: 0, y: 0}
+  move('left');
+  console.log(position); // { x: -1, y: 0}
+  move('right');
+  console.log(position); // { x: 0, y: 0}
+}
