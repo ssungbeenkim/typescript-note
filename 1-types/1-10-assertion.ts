c6f60d33eb8eaf9e8192d01a8abfca232a4a894a
wandakim <nwd1583@naver.com>
2023-03-14 14:42:39 +0900

    Types: assertion

--- 1-types/1-10-assertion.ts
+++ 1-types/1-10-assertion.ts
@@ -1,22 +1,26 @@
 {
   /**
    * Type Assertions 💩
+   * 쓰지 않는 것이 좋지만 JS와 함께 쓰기 때문에 불가피하게 써야 하는 경우도 있다.
    */
   function jsStrFunc(): any {
-    return 2;
+    return 'string'; // 숫자를 할당하게 되면 에러가 발생하지 않지만 문제가 발생할 수 있음
   }
   const result = jsStrFunc();
   console.log((result as string).length);
-  console.log((<string>result).length);
+  // result가 string type인 것을 알고 있으므로 casting을 해 주어 string api들을 사용할 수 있다.
+  console.log((<string>result).length); // 위와 동일
 
   const wrong: any = 5;
-  console.log((wrong as Array<number>).push(1)); // 😱
+  console.log((wrong as Array<number>).push(1));
+  // 😱 에러 발생, 어플리케이션 종료
 
   function findNumbers(): number[] | undefined {
     return undefined;
   }
   const numbers = findNumbers()!;
+  // numbers! 이렇게 해도 된다. 배열이 들어있는 것을 정말로 확신할 수 있는 경우에만 쓴다.
   numbers.push(2); // 😱
 
-  const button = document.querySelector('class')!;
+  const button = document.querySelector('class')!; // 100%일 때는 이렇게 쓸 수 있음.
 }
