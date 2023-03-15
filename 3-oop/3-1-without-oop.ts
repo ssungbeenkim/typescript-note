81d47f4ba1f8692725398df4868b4caf14274f70
wandakim <nwd1583@naver.com>
2023-03-15 15:35:07 +0900

    OOP: notes init

--- 3-oop/3-1-without-oop.ts
+++ 3-oop/3-1-without-oop.ts
@@ -4,21 +4,21 @@
     hasMilk: boolean;
   };
 
-  const BEANS_GRAMM_PER_SHOT: number = 7;
+  const BEANS_GRAM_PER_SHOT: number = 7;
 
   let coffeeBeans: number = 0;
   function makeCoffee(shots: number): CoffeeCup {
-    if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
+    if (coffeeBeans < shots * BEANS_GRAM_PER_SHOT) {
       throw new Error('Not enough coffee beans!');
     }
-    coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
+    coffeeBeans -= shots * BEANS_GRAM_PER_SHOT;
     return {
       shots,
       hasMilk: false,
     };
   }
 
-  coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
+  coffeeBeans += 3 * BEANS_GRAM_PER_SHOT;
   const coffee = makeCoffee(2);
   console.log(coffee);
 }
