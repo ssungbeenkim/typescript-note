3f1bf42dfdb846580acf3d39ae218e1619673379
wandakim <nwd1583@naver.com>
2023-03-29 20:25:01 +0900

    10.8 Readonly

--- 9-advanced/9-5-readonly.ts
+++ 9-advanced/9-5-readonly.ts
@@ -3,8 +3,8 @@
     title: string;
     description: string;
   };
-
+  // 공통적으로 쓰이는 타입들은 만들어져 있다. (Utility Type)
   function display(todo: Readonly<ToDo>) {
-    // todo.title = 'jaja';
+    // todo.title = 'jaja';// -> 보여주기만 하는 함수가 값을 업데이트 하는것을 방지
   }
 }
