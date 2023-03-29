a1bf6109d3349fbbc77b9b6840c856ee5d9e0b9f
wandakim <nwd1583@naver.com>
2023-03-29 20:30:50 +0900

    10.9 Partial Type

--- 9-advanced/9-6-partial.ts
+++ 9-advanced/9-6-partial.ts
@@ -5,9 +5,9 @@
     label: string;
     priority: 'high' | 'low';
   };
-
+  // 많이 사용되는 예
   function updateTodo(todo: ToDo, fieldsToUpdate: Partial<ToDo>): ToDo {
-    return { ...todo, ...fieldsToUpdate };
+    return { ...todo, ...fieldsToUpdate }; // ToDo 타입에 있는 것 중에서 부분적으로 허용할 수 있다.
   }
   const todo: ToDo = {
     title: 'learn TypeScript',
