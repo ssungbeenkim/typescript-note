fa05569d931d2b02ee90293752f966c4765b51d2
wandakim <nwd1583@naver.com>
2023-03-29 19:15:47 +0900

    9.4 error handling

--- 8-exception/8-2-handling.ts
+++ 8-exception/8-2-handling.ts
@@ -10,10 +10,9 @@
 
   class UserService {
     constructor(private client: NetworkClient) {}
-
     login() {
-      this.client.tryConnect();
-    }
+      this.client.tryConnect(); // 처음으로 사용하는 곳은 이곳이다. 여기서 핸들링?
+    } // -> 에러가 발생했을 때 내가 정확하게 처리할 수 있는 것이 아니라면 캐치하지 않는 것이 낫다!
   }
 
   class App {
@@ -23,6 +22,7 @@
         this.userService.login();
       } catch (error) {
         // show dialog to use
+        // 처리할 수 있는 곳에서 캐치해 주는 것이 좋다. 조금 더 의미있는 에러 처리를 할 수 있다.
       }
     }
   }
@@ -30,5 +30,5 @@
   const client = new NetworkClient();
   const service = new UserService(client);
   const app = new App(service);
-  app.run();
+  app.run(); // 호출 순서와 함께 에러 발생
 }
