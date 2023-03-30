f0ddf919b38a33b7057e97720f2f6bd1e7cceeac
wandakim <nwd1583@naver.com>
2023-03-30 18:21:22 +0900

    10.12 Record

--- 9-advanced/9-9-record.ts
+++ 9-advanced/9-9-record.ts
@@ -4,6 +4,8 @@ type PageInfo = {
 type Page = 'home' | 'about' | 'contact';
 
 const nav: Record<Page, PageInfo> = {
+  //*
+  // 하나를 key로 쓰고 다른 하나를 다른 타입으로 묶고 싶을 때 쓸 수 있다.
   home: { title: 'Home' },
   about: { title: 'About' },
   contact: { title: 'Contact' },
